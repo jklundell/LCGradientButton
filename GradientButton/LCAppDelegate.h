0d0e44ba76a7d710488b8d230772b6960f4205ce
Jonathan Lundell <jlundell@pobox.com>
2012-01-10 11:35:53 -0800

    Initial Commit

---
+++ GradientButton/LCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  LCAppDelegate.h
+//  GradientButton
+//
+//  Created by Jonathan Lundell on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LCViewController;
+
+@interface LCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) LCViewController *viewController;
+
+@end
---
+++ GradientButton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GradientButton
+//
+//  Created by Jonathan Lundell on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+    }
+}
